f1ab000ffe93c9e8dc28300ce1a7c801b6881c1b
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-27 01:32:47 +0530

    Refactor largest number finder to comment out user input and approaches

--- 01basics/largestNumber.java
+++ 01basics/largestNumber.java
@@ -1,22 +1,36 @@
-import java.util.*;
 
 public class largestNumber{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
+        System.out.println("------------------");
 
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
+        // int a = in.nextInt();
+        // int b = in.nextInt();
+        // int c = in.nextInt();
 
-        int max = a;
-        if (b > max) {
-            max = b;
-        }
+        // FIRST APPROACH
+        // int max = a;
+        // if (b > max) {
+        //     max = b;
+        // }
 
-        if (c > max) {
-            max = c;
-        }
+        // if (c > max) {
+        //     max = c;
+        // }
 
-        System.out.println(max);
+        // SECOND APPROACH
+        // int max = 0;
+
+        // if (a > b) {
+        //     max = a;
+        // } else {
+        //     max = b;
+        // }
+
+        // if (c > max) {
+        //     max = c;
+        // }
+
+        System.out.println(Math.max(34, 32));
     }
 }
